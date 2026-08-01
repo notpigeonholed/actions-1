762fd2efc1c889278a2156f4ea181c9f88e68cbe
notpigeonholed <37675091+notpigeonholed@users.noreply.github.com>
2026-08-01 12:02:45 +0530

    created a new script for ascii-art

---
+++ ascii-script.sh
@@ -0,0 +1,9 @@
+sudo apt-get install cowsay -y
+
+cowsay -f dragon "I'm Smaug, the Dragon of the Lonely mountain and I was voiced by Benedict Cumberbatch, none other than Sherlock Strange himself" >> dragon.txt
+
+grep -i "dragon" dragon.txt
+
+cat dragon.txt
+
+ls -lrta
